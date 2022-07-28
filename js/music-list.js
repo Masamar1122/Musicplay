863b89f79424e513b95f7b1c70bb586812c60912
Masamar1122 <110186450+Masamar1122@users.noreply.github.com>
2022-07-28 04:54:26 -1200

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,362 @@
+let allMusic = [
+  {
+    name: "Forever Young",
+    artist: "Tak dikenali",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Night Changes",
+    artist: "One Direction",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Something Just Like This",
+    artist: "Codplay",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Don't Be Shy",
+    artist: "Tiesto & Karol G",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Lily",
+    artist: "Alan Walker",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Lost Sky - Vision NCS",
+    artist: "NCS Release",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Let Me Down Slowly",
+    artist: "Alec Benjamin",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Beautiful In White",
+    artist: "Shane Filan",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Hymm For The Weekend",
+    artist: "Coldplay",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Nothing's Gonna Change",
+    artist: "Tak dikenali",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "Berkibarlah Bendera Negeriku",
+    artist: "Shanna Shannon",
+    img: "music-11",
+    src: "music-11"
+  },
+  {
+    name: "Silhouette",
+    artist: "Kanna Boon",
+    img: "music-12",
+    src: "music-12"
+  },
+  {
+    name: "Cinta Sampai Mati",
+    artist: "Raffa Affar",
+    img: "music-13",
+    src: "music-13"
+  },
+  {
+    name: "Skinnyfabs - Happy",
+    artist: "Marsya Ardini Cover",
+    img: "music-14",
+    src: "music-14"
+  },
+  {
+    name: "Fire On Fire",
+    artist: "Sam Smith",
+    img: "music-15",
+    src: "music-15"
+  },
+  {
+    name: "Bring Me To Life",
+    artist: "Evanescence",
+    img: "music-16",
+    src: "music-16"
+  },
+  {
+    name: "Hero Cash Cash",
+    artist: "Tak dikenali",
+    img: "music-17",
+    src: "music-17"
+  },
+  {
+    name: "Gustixa - Spirit",
+    artist: "Gustixa & Shalom Margaret",
+    img: "music-18",
+    src: "music-18"
+  },
+  {
+    name: "Gustixa - Fallen Kingdom",
+    artist: "Gustixa & Shalom Margaret",
+    img: "music-19",
+    src: "music-19"
+  },
+  {
+    name: "Gustixa - Lemon Tree",
+    artist: "Gustixa",
+    img: "music-20",
+    src: "music-20"
+  },
+  {
+    name: "Olivia Rodrigo - Happier",
+    artist: "Olivia Rodrigo",
+    img: "music-21",
+    src: "music-21"
+  },
+  {
+    name: "Lil Nas X - Montero",
+    artist: "Lil Nas",
+    img: "music-22",
+    src: "music-22"
+  },
+  {
+    name: "Leave Before You Love Me",
+    artist: "Marshmello x Jonas Brothers",
+    img: "music-23",
+    src: "music-23"
+  },
+  {
+    name: "Lisa - Money x Lalisa",
+    artist: "Lisa",
+    img: "music-24",
+    src: "music-24"
+  },
+  {
+    name: "On The Floor ft. Pitbull",
+    artist: "Jennifer Lopez",
+    img: "music-25",
+    src: "music-25"
+  },
+  {
+    name: "Tove Lo - Habits",
+    artist: "Tove Lo",
+    img: "music-26",
+    src: "music-26"
+  },
+  {
+    name: "Demi Lovato - Heart Attack",
+    artist: "Demi Lovato",
+    img: "music-27",
+    src: "music-27"
+  },
+  {
+    name: "7!! Orange",
+    artist: "SevenOopsVEVO",
+    img: "music-28",
+    src: "music-28"
+  },
+  {
+    name: "Kaleb J - It's Only Me",
+    artist: "Kaleb J",
+    img: "music-29",
+    src: "music-29"
+  },
+  {
+    name: "Bella Poarch - Build A Bitch",
+    artist: "Bella Poarch",
+    img: "music-30",
+    src: "music-30"
+  },
+  {
+    name: "INFERNO",
+    artist: "Sub Urban & Bella Poarch",
+    img: "music-31",
+    src: "music-31"
+  },
+  {
+    name: "Emergency",
+    artist: "Remix RMX",
+    img: "music-32",
+    src: "music-32"
+  },
+  {
+    name: "Somebody That I Used To Now",
+    artist: "Tak dikenal",
+    img: "music-33",
+    src: "music-33"
+  },
+  {
+    name: "DJ Without Me",
+    artist: "Halsey",
+    img: "music-34",
+    src: "music-34"
+  },
+  {
+    name: "The Final Countdown",
+    artist: "Europe",
+    img: "music-35",
+    src: "music-35"
+  },
+  {
+    name: "DJ On My Way",
+    artist: "Alan Walker",
+    img: "music-36",
+    src: "music-36"
+  },
+  {
+    name: "Party Favor",
+    artist: "Billie Eilish",
+    img: "music-37",
+    src: "music-37"
+  },
+  {
+    name: "Katie Sky - Monsters",
+    artist: "Katie Sky",
+    img: "music-38",
+    src: "music-38"
+  },
+  {
+    name: "Dj Mati Matian",
+    artist: "Tak dikenali",
+    img: "music-39",
+    src: "music-39"
+  },
+  {
+    name: "The Lazy Song",
+    artist: "Bruno Mars",
+    img: "music-40",
+    src: "music-40"
+  },
+  {
+    name: "To The Bone",
+    artist: "Pamungkas",
+    img: "music-41",
+    src: "music-41"
+  },
+  {
+    name: "Side To Side",
+    artist: "Ariana Grande",
+    img: "music-42",
+    src: "music-42"
+  },
+  {
+    name: "Alone",
+    artist: "Alan Walker",
+    img: "music-43",
+    src: "music-43"
+  },
+  {
+    name: "Fly Me To The Moon",
+    artist: "Frank Sinatra",
+    img: "music-44",
+    src: "music-44"
+  },
+  {
+    name: "So Am I",
+    artist: "Ava Max",
+    img: "music-45",
+    src: "music-45"
+  },
+  {
+    name: "Sweet But Psycho",
+    artist: "Ava Max",
+    img: "music-46",
+    src: "music-46"
+  },
+  {
+    name: "Roar",
+    artist: "Katy Perry",
+    img: "music-47",
+    src: "music-47"
+  },
+  {
+    name: "One Day",
+    artist: "Tak dikenal",
+    img: "music-48",
+    src: "music-48"
+  },
+  {
+    name: "All The Things She said",
+    artist: "t.A.T.u",
+    img: "music-49",
+    src: "music-49"
+  },
+  {
+    name: "How do you do",
+    artist: "Dj TikTok",
+    img: "music-50",
+    src: "music-50"
+  },
+  {
+    name: "Biarkan orang tertawa",
+    artist: "Dexsen Remixer",
+    img: "music-51",
+    src: "music-51"
+  },
+  {
+    name: "Dj Funkot Meneketehe",
+    artist: "Tak dikenal",
+    img: "music-52",
+    src: "music-52"
+  },
+  {
+    name: "Famy ava speed up",
+    artist: "Headset lyrics",
+    img: "music-53",
+    src: "music-53"
+  },
+  {
+    name: "Live it up",
+    artist: "Nicky jam ft Will smith",
+    img: "music-54",
+    src: "music-54"
+  },
+  {
+    name: "Behind the mask",
+    artist: "Tak dikenal",
+    img: "music-55",
+    src: "music-55"
+  },
+  {
+    name: "Mencari alasan",
+    artist: "Raffa Badri",
+    img: "music-56",
+    src: "music-56"
+  },
+  {
+    name: "Safe and sound",
+    artist: "Rebulation music",
+    img: "music-57",
+    src: "music-57"
+  },
+  {
+    name: "Stars breakbeat",
+    artist: "Tak dikenal",
+    img: "music-58",
+    src: "music-58"
+  },
+  {
+    name: "Until i found you",
+    artist: "Stephen sanchez",
+    img: "music-59",
+    src: "music-59"
+  },
+  {
+    name: "Without you",
+    artist: "Tak dikenal",
+    img: "music-60",
+    src: "music-60"
+  },
+];
